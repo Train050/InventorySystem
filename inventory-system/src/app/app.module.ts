7484c3db074aa287585f67b25bb0e466eb14951e
Gabriell Yap <g.yap@ufl.edu>
2023-02-07 16:30:13 -0500

    added login page

--- inventory-system/src/app/app.module.ts
+++ inventory-system/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,6 +19,7 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     MatGridListModule,
     BrowserAnimationsModule,
--- inventory-system/src/app/pages/login-page/login-page.component.ts
+++ inventory-system/src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
+  username = "";
+  password = "";
+  errorMsg = "";
 
+  constructor(private auth: AuthService, private router: Router) { }
+  ngOnInit(): void {
+  }
+
+  login() {
+    if (this.username.trim().length === 0) {
+        this.errorMsg = "Username is required";
+    } else if (this.password.trim().length === 0) {
+        this.errorMsg = "Password is required";
+    } else {
+      this.errorMsg = "";
+      let res = this.auth.login(this.username, this.password);
+      if (res === 200) {
+        this.router.navigate(['home'])
+      }
+      if (res === 403) {
+        this.errorMsg = "Invalid Credentials";
+      }
+    }
+  }
 }
