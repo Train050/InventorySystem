914225e07e94b5eef60685edd30c9b97d0c0cc6b
Gabriell Yap <g.yap@ufl.edu>
2023-02-07 19:50:02 -0500

    Added table to inventory page

--- inventory-system/src/app/app.module.ts
+++ inventory-system/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { LandingPageComponent } from './pages/landing-page/landing-page.componen
 import { InventoryHomePageComponent } from './pages/inventory-home-page/inventory-home-page.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatGridListModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatMenuModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent],
