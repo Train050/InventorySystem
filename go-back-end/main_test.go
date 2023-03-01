e6c67c10a5388171b8c23ec6596de37be73eaf64
Nicholas Sardinia <nicholassardinia@ufl.edu>
2023-02-28 19:07:27 -0500

    Added database connection to backend tests

--- go-back-end/main_test.go
+++ go-back-end/main_test.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"testing"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+// Setup test database
+func TestMain(m *testing.M) {
+	code, err := run(m)
+	if err != nil {
+		fmt.Println(err)
+	}
+	os.Exit(code)
+}
+
+func run(m *testing.M) (code int, err error) {
+	//Create test database
+	//If we cannot connect to the database, return an error
+	db, err = gorm.Open(sqlite.Open("inventory.db"), &gorm.Config{})
+	if err != nil {
+		return -1, fmt.Errorf("could not connect to the database: %w", err)
+	}
+
+	//Run tests
+	return m.Run(), nil
+}
+
+// Testing functions
 // User tests
 func TestInsertUser(t *testing.T) {
 	result := 1 + 3
-
 	if result != 4 {
-		t.Errorf("Add(1, 3) Failed")
+		t.Errorf("Add failed")
 	} else {
-		t.Logf("Add(1, 3) PASSED")
+		t.Logf("Add passed")
 	}
 }
-
 func TestUpdateUser(t *testing.T) {
 
 }
