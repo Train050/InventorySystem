0d6020dae899b641a700a42b3773b4982cc0ced7
Gabriell Yap <g.yap@ufl.edu>
2023-04-19 19:03:00 -0400

    added cypress test for registration

--- inventory-system/cypress/e2e/login.cy.ts
+++ inventory-system/cypress/e2e/login.cy.ts
@@ -1,14 +1,8 @@
-describe('Login Test', () => {
-  it('Logs in and redirects to inventory page', () => {
-    cy.visit('http://localhost:4200/login-page') // Replace with your login page URL
-
-    // Enter username and password and submit the form
-    cy.get('user').type('Username')
-    cy.get('#password').type('mypassword')
-    cy.get('button[type="submit"]').click()
-
-    // Wait for the login to complete and redirect to inventory page
-    cy.url().should('http://localhost:4200/inventory-home-page') // Replace with your inventory page URL
-  })
-})
+describe('redirect to inventory page from registration page', () => {
+  it('should redirect to inventory page', () => {
+    cy.visit('http://localhost:4200/register')
+    cy.get('.mat-mdc-button').click({ multiple: true})
+    cy.visit('http://localhost:4200/inventory-home-page')
+  });
+});
 
